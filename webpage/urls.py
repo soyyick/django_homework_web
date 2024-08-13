61e98b1c949341ead10d3e6dfadbc08be4562952
soyyick <gunmy12gun@gmail.com>
2024-08-13 13:17:24 +0700

    add color

--- webpage/urls.py
+++ webpage/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.indexPage, name="index"),
     path("about/", views.aboutPage, name="about"),
     path("contact/", views.contactPage, name="contact"),
+    path("table/", views.tablePage, name="table"),
+    path("card/", views.cardPage, name="card"),
+    path("color/", views.cardColorPage, name="color"),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -13,3 +13,30 @@ def aboutPage(request):
 
 def contactPage(request):
     return render(request, 'contact.html')
+
+
+def tablePage(request):
+    context = {}
+    lt = list(range(0, 100))
+    context["list"] = lt
+
+    return render(request, 'table.html', context)
+
+
+def cardPage(request):
+    context = {}
+    lt = list(range(0, 100))
+    context["list"] = lt
+
+    return render(request, 'card.html', context)
+
+
+def cardColorPage(request):
+    context = {
+        'color': '',
+    }
+
+    if request.method == "GET":
+        context['color'] = request.GET.get('color')
+
+    return render(request, 'color.html', context)
